9ee37324f1ade6c342d4389232f996ee10ffb8d4
LHB <lhb@LHBdeMacBook-Pro.local>
2016-08-15 15:12:16 +0800

    自定义打印

---
+++ LHBExtension-Swift/LHBExtension-Swift/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  LHBExtension-Swift
+//
+//  Created by LHB on 16/8/15.
+//  Copyright © 2016年 LHB. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        LHBPrint("吃饭了吗")
+        return true
+    }
+}
+
+///全局函数，自定义打印
+
+func LHBPrint<T>(message : T,className : String = #file,funcName : String = #function,lineNum : Int = #line) -> Void{
+    let className = (className as NSString).lastPathComponent.stringByReplacingOccurrencesOfString(".swift", withString:"")
+    //print("该类:\(className)---该方法:\(funcName)---行数:\(lineNum)---打印信息:\(message)")
+    print("类名:\(className)---行数:\(lineNum)---打印信息:\(message)")
+}
---
+++ LHBExtension-Swift/LHBExtension-Swift/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LHBExtension-Swift
+//
+//  Created by LHB on 16/8/15.
+//  Copyright © 2016年 LHB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+        LHBPrint("hello swift")
+    }
+
+
+}
+
